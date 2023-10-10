9a7d0ef7dc7fdcebc9a3a289237664c568f1c9d0
Kyymo <137647536+MehdiAlani@users.noreply.github.com>
2023-10-10 15:26:06 -0200

    10 oct 23

---
+++ 71A.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int N , SizeOfWord,j,i=0;
+    do{
+        scanf(" %d",&N);
+    }while(N<0 || N > 100);
+
+    unsigned char c;
+    unsigned char ListWord[100][N];
+
+    for(j=0; j < N ;j++)
+        for(int i = 0; i < 100; i++) ListWord[j][i]='\0';
+
+    for(int j =0; j <= N; j++){
+    i=0;
+    c = getchar();
+    while(c !='\n'){
+        ListWord[j][i]= c;
+        c = getchar();
+        i++;
+    }
+    }
+    printf("\nYour Output1:\n");
+    for(j=0; j <= N ;j++) {
+        for(int i = 0; ListWord[j][i] != '\0'; i++) {
+        printf("%c",ListWord[j][i]);
+        }
+        printf("\n");
+    }
+    printf("\nYour Output2:\n");
+
+    for(j=0; j <= N ;j++){
+        for(int i = 0; ListWord[j][i] != '\0'; i++);
+        SizeOfWord = i-1;
+        if(SizeOfWord > 10) printf("%c%d%c",ListWord[j][0],SizeOfWord-2,ListWord[j][SizeOfWord-2]);
+        else{
+            for(int k = 0; k <= i ; k++){
+                printf("%c",ListWord[j][i]);
+            }
+        }
+        printf("    :%d",SizeOfWord);
+        printf("\n");
+    }
+    return 0;
+}
