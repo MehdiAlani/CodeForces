4234dc44bd7be3e26151c76e5dd226a663ca3752
Kyymo <137647536+MehdiAlani@users.noreply.github.com>
2023-10-13 22:00:13 -0200

    13 oct 23

--- 112.c
+++ 112.c
@@ -5,9 +5,21 @@
 
 
 int main(){
-    
-
-
-
+    char Str1[100],Str2[100];
+    scanf("%s",&Str1);
+    scanf("%s",&Str2);
+    short int State = 1;
+    for(int i = 0; Str1[i]!='\0'; i++){
+        if(Str1[i]==Str2[i] || Str1[i]==Str2[i]+32 || Str1[i]==Str2[i]-32) continue;
+        else if(Str1[i] > Str2[i]) {
+            State = 0;
+            break;
+        }
+        else{
+            State = -1;
+            break;
+        }
+    }
+    printf("%d",State);
     return 0;
 }
