44bced4183ecd1cf1179a8074b972c7886eea0e7
Kyymo <137647536+MehdiAlani@users.noreply.github.com>
2023-11-08 22:59:38 -0200

    8 nov 23

---
+++ 1882.c
@@ -0,0 +1,11 @@
+// https://codeforces.com/problemset/problem/1882/
+
+
+#include <stdio.h>
+
+int main(){
+
+
+
+    return 0;
+}
---
+++ 677.c
@@ -0,0 +1,12 @@
+// https://codeforces.com/problemset/problem/677/A
+
+
+
+#include <stdio.h>
+
+int main(){
+
+
+
+    return 0;
+}
